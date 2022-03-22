fbdbb7afb29bd30af08dea58a18a95f7a0aea49f
Łukasz Świtalski <lukaz.switalski@gmail.com>
2022-03-22 13:05:27 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('v1');
+
+  const port = process.env.PORT || 5000;
+  await app.listen(port, () => {
+    console.log(`
+      =================================================================
+            App started at port ${port}
+      =================================================================
+    `);
+  });
+}
+bootstrap();
